c17715cfa609e7e5d11cedbe1e4162f16b7b8de7
Confidence Monday <confidencemonday142@gmail.com>
2026-05-15 17:24:04 +0100

    Update wdio.conf.js for emulator configuration

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,24 +1,30 @@
-exports.config = {
-  runner: 'local',
-  port: 4723,
-  specs: ['./test/specs/**/*.js'],
-  maxInstances: 1,
-  capabilities: [{
-    platformName: 'Android',
-    'appium:deviceName': 'SM-N97OU',
-    'appium:udid': '098903739R000019',
-    'appium:platformVersion': '12',
-    'appium:automationName': 'UiAutomator2',
-    'appium:app': 'C:\\Users\\USER\\Documents\\swaglabs-mobile\\Android.SauceLabs.Mobile.Sample.app.2.7.1.apk',
-    'appium:appPackage': 'com.swaglabsmobileapp',
-    'appium:appActivity': 'com.swaglabsmobileapp.MainActivity',
-    'appium:noReset': true
-  }],
-  logLevel: 'info',
-  framework: 'mocha',
-  reporters: ['spec'],
-  mochaOpts: {
-    ui: 'bdd',
-    timeout: 60000
-  }
-}
+import path from "pathe";
+
+export const config = {
+    hostname: '127.0.0.1',
+    port: 4723,
+    path: '/',
+    capabilities: [{
+        platformName: 'Android',
+        'appium:deviceName': 'Android Emulator',
+        'appium:udid': 'emulator-5554',
+        'appium:automationName': 'UiAutomator2',
+        'appium:app': './Android.SauceLabs.Mobile.Sample.app.2.7.1.apk',
+        'appium:appWaitActivity': '*',
+        'appium:newCommandTimeout': 300,
+        'appium:uiautomator2ServerLaunchTimeout': 60000,
+        'appium:androidInstallTimeout': 120000,
+        'appium:appWaitDuration': 60000,
+        'appium:autoGrantPermissions': true
+    }],
+    framework: 'mocha',
+    specs: ['./test/specs/**/*.js'],
+    exclude:[],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 120000
+    },
+    reporters: ['spec'],
+    logLevel: 'info',
+    services: ['appium'],
+};
